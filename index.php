226d99ddd976d31ea15e401148699405f40e0d9c
Rohan5202 <5202rohan@gmail.com>
2025-04-18 13:22:40 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
 <html>
-<body>
- 
-<?php
-echo "test script azure!";
-?>
-
-</body>
+ <head>
+  <title>PHP-Test</title>
+ </head>
+ <body>
+  <?php echo '<h1>Hello World!</h1><h3>Welcome to WPMU DEV</h3>'; ?>
+ </body>
 </html>
